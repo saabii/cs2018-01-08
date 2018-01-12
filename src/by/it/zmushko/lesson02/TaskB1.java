9df6d3bd5ef0d73793c630924c8e1c3a4a32424b
ZmushkoPavel <hunter19_87@mail.ru>
2018-01-12 12:43:21 +0300

    complete

--- src/by/it/zmushko/lesson02/TaskB1.java
+++ src/by/it/zmushko/lesson02/TaskB1.java
@@ -22,7 +22,7 @@ class TaskB1 {
     public static void main(String[] args) {
         System.out.print ("Выводим квадрат числа") ;
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt() ;
+        int a = sc.nextInt();
                 System.out.println(a*a);
     }
 }
