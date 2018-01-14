b959aa135a2b1571cd52008162c8737093e50d09
Khmelov <375336849110@tut.by>
2018-01-14 23:42:23 +0300

    fix bugs

--- src/by/it/borovik/lesson02/TaskC1.java
+++ src/by/it/borovik/lesson02/TaskC1.java
@@ -15,7 +15,7 @@ Sum = 26
 class TaskC1 {
     public static void main(String[] args) {
         System.out.print("Введите клавиатурой число 34");
-        Scanner a = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         System.out.print("Введите клавиатурой число 26");
         int b = scan.nextInt();
