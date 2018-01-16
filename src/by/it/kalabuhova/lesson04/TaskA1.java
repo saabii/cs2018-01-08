476f30cc4c0d2eaa07e9232d3dfd81b163e98ad7
marilizka <kalabuhova@yandex.ru>
2018-01-16 16:53:45 +0300

    a1,a2,a3,b1,b2 - complete

--- src/by/it/kalabuhova/lesson04/TaskA1.java
+++ src/by/it/kalabuhova/lesson04/TaskA1.java
@@ -26,12 +26,14 @@ package by.it.kalabuhova.lesson04;
  */
 public class TaskA1 {
 
-//    public static void main(String[] args) {
-//
-//for (int i=1; i<10; i++) {
- //   System.out.println("Я изучаю программирование.");
-//}
-  //      System.out.println("Меня интересует высокая зарплата!");
-   // }
+    public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i++) {
+            if (i != 3 && i != 9) {
+                System.out.println("Я изучаю программирование.");
+            } else {
+                System.out.println("Меня интересует высокая зарплата!");
+            }
+        }
+    }
 }
--- src/by/it/kalabuhova/lesson04/TaskA3.java
+++ src/by/it/kalabuhova/lesson04/TaskA3.java
@@ -22,6 +22,21 @@ package by.it.kalabuhova.lesson04;
  */
 
 
-public class TaskA3 {
+import java.util.Scanner;
 
+public class TaskA3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b) {
+            if (b == c) System.out.println(a + " " + b + " " + c);
+            else System.out.println(a + " " + b);
+        } else {
+            if (a == c) System.out.println(a + " " + c);
+            else if (b == c) System.out.println(b + " " + c);
+        }
+    }
 }
