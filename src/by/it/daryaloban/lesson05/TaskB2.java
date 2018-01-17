c0b36854825c890bc471583e78c9eb6a3edac149
DaryaLoban <daryalobanhere@gmail.com>
2018-01-18 01:09:17 +0300

    Done Lesson02:A123,B123,C123 Lesson03:A12,B12,C12 Lesson04:A123,B123,C123 Lesson05:A12,B12,C12
    ToDo
    Lesson02:-
    Lesson03:-
    Lesson04:-
    Lesson05:-

--- src/by/it/daryaloban/lesson05/TaskB2.java
+++ src/by/it/daryaloban/lesson05/TaskB2.java
@@ -20,6 +20,37 @@ b=[11, 22, 33, 44, 55, 66, 77, 88, 99, 0]
 
 */
 
-public class TaskB2  {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+
+public class TaskB2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[20];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.valueOf(reader.readLine());
+        }
+
+        int[] arr1 = new int[10];
+        int[] arr2 = new int[10];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[i];
+            arr2[i] = arr[i + arr2.length];
+        }
+
+        for (int i : arr1) {
+            System.out.println("a=" + Arrays.toString(arr1));
+            break;
+        }
+
+        for (int i : arr2) {
+            System.out.println("b=" + Arrays.toString(arr2));
+            break;
+        }
+    }
 }
+
