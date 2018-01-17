04f9783db6bc4bdad8cea27045c9317b6f605a96
DaryaLoban <daryalobanhere@gmail.com>
2018-01-18 00:37:42 +0300

    Done Lesson02:A123,B123,C123 Lesson03:A12,B12,C12 Lesson04:A123,B123,C123 Lesson05:A12,B1,C12
    ToDo
    Lesson02:-
    Lesson03:-
    Lesson04:-
    Lesson05:B2

--- src/by/it/daryaloban/lesson04/TaskC3.java
+++ src/by/it/daryaloban/lesson04/TaskC3.java
@@ -25,208 +25,92 @@ package by.it.daryaloban.lesson04;
 
 public class TaskC3 {
     public static void main(String[] args) {
-        int i, j, mul;
-        for (i = 1; i <= 10; i++) {
-            j = 1;
-            for (j = 1; j <= 10; j++) {
-                mul = i * j;
-                if (i != 1 && j != 1) System.out.println(convertToStringI(i) + " умножить на " + convertToStringJ(j) +
-                        " равно " + convertToStringMul(mul));
-                else continue;
 
+        for (int i = 2; i <= 10; i++) {
+            for (int b = 2; b <= 10; b++) {
+                int y = b * i;
+                System.out.println(intToString(i) + " умножить на " + intToString(b) + " равно " + intToString(y) + " ");
             }
         }
     }
 
-    public static String convertToStringI(int i) {
-        String str1 = String.valueOf(i);
+    private static String intToString(int a) {
+        if (a >= 1 && a <= 19) {
 
-        switch (str1) {
-            case "2":
-                str1 = "два";
-                break;
-            case "3":
-                str1 = "три";
-                break;
-            case "4":
-                str1 = "четыре";
-                break;
-            case "5":
-                str1 = "пять";
-                break;
-            case "6":
-                str1 = "шесть";
-                break;
-            case "7":
-                str1 = "семь";
-                break;
-            case "8":
-                str1 = "восемь";
-                break;
-            case "9":
-                str1 = "девять";
-                break;
-            case "10":
-                str1 = "деcять";
-                break;
+            switch (a) {
+                case 1:
+                    return "один";
+                case 2:
+                    return "два";
+                case 3:
+                    return "три";
+                case 4:
+                    return "четыре";
+                case 5:
+                    return "пять";
+                case 6:
+                    return "шесть";
+                case 7:
+                    return "семь";
+                case 8:
+                    return "восемь";
+                case 9:
+                    return "девять";
+                case 10:
+                    return "десять";
+                case 11:
+                    return "одиннадцать";
+                case 12:
+                    return "двенадцать";
+                case 13:
+                    return "тринадцать";
+                case 14:
+                    return "четырнадцать";
+                case 15:
+                    return "пятнадцать";
+                case 16:
+                    return "шестнадцать";
+                case 17:
+                    return "семнадцать";
+                case 18:
+                    return "восемнадцать";
+                case 19:
+                    return "девятнадцать";
+                default:
+                    return " ";
+            }
+
+        } else if (a >= 20 && a <= 99) {
+            switch (a) {
+                case 20:
+                    return "двадцать";
+                case 30:
+                    return "тридцать";
+                case 40:
+                    return "сорок";
+                case 50:
+                    return "пятьдесят";
+                case 60:
+                    return "шестьдесят";
+                case 70:
+                    return "семьдесят";
+                case 80:
+                    return "восемьдесят";
+                case 90:
+                    return "девяносто";
+                default: {
+                    int b = a - a % 10;
+                    int c = a % 10;
+                    return intToString(b) + " " + intToString(c);
+                }
+            }
+
+        } else if (a == 100) {
+            return "сто";
+        } else {
+            return "";
         }
-        return str1;
-
-
-    }
-
-    public static String convertToStringJ(int j) {
-        String str2 = String.valueOf(j);
-        switch (str2) {
-            case "2":
-                str2 = "два";
-                break;
-            case "3":
-                str2 = "три";
-                break;
-            case "4":
-                str2 = "четыре";
-                break;
-            case "5":
-                str2 = "пять";
-                break;
-            case "6":
-                str2 = "шесть";
-                break;
-            case "7":
-                str2 = "семь";
-                break;
-            case "8":
-                str2 = "восемь";
-                break;
-            case "9":
-                str2 = "девять";
-                break;
-            case "10":
-                str2 = "деcять";
-                break;
-
-
-        }
-
-        return str2;
-    }
-
-    public static String convertToStringMul(int mul) {
-        String str_mul = String.valueOf(mul);
-        switch (str_mul) {
-            case "4":
-                str_mul = "четыре";
-                break;
-            case "6":
-                str_mul = "шесть";
-                break;
-            case "8":
-                str_mul = "восемь";
-                break;
-            case "9":
-                str_mul = "девять";
-                break;
-            case "10":
-                str_mul = "деcять";
-                break;
-            case "12":
-                str_mul = "двенадцать";
-                break;
-            case "14":
-                str_mul = "четырнадцать";
-                break;
-            case "15":
-                str_mul = "пятнадцать";
-                break;
-            case "16":
-                str_mul = "шестнадцать";
-                break;
-            case "18":
-                str_mul = "восемнадцать";
-                break;
-            case "20":
-                str_mul = "двадцать";
-                break;
-            case "21":
-                str_mul = "двадцать один";
-                break;
-            case "24":
-                str_mul = "двадцать четыре";
-                break;
-            case "25":
-                str_mul = "двадцать пять";
-                break;
-            case "27":
-                str_mul = "двадцать семь";
-                break;
-            case "28":
-                str_mul = "двадцать восемь";
-                break;
-            case "30":
-                str_mul = "тридцать";
-                break;
-            case "32":
-                str_mul = "тридцать два";
-                break;
-            case "35":
-                str_mul = "тридцать пять";
-                break;
-            case "36":
-                str_mul = "тридцать шесть";
-                break;
-            case "40":
-                str_mul = "сорок";
-                break;
-            case "42":
-                str_mul = "сорок два";
-                break;
-            case "45":
-                str_mul = "сорок пять";
-                break;
-            case "48":
-                str_mul = "сорок восемь";
-                break;
-            case "49":
-                str_mul = "сорок девять";
-                break;
-            case "50":
-                str_mul = "пятьдесят";
-                break;
-            case "54":
-                str_mul = "пятьдесят четыре";
-                break;
-            case "56":
-                str_mul = "пятьдесят шесть";
-                break;
-            case "60":
-                str_mul = "шестьдесят";
-                break;
-            case "63":
-                str_mul = "шестьдесят три";
-                break;
-            case "64":
-                str_mul = "шестьдесят четыре";
-                break;
-            case "70":
-                str_mul = "семьдесят";
-                break;
-            case "72":
-                str_mul = "семьдесят два";
-                break;
-            case "80":
-                str_mul = "восемьдесят";
-                break;
-            case "81":
-                str_mul = "восемьдесят один";
-                break;
-            case "90":
-                str_mul = "девяносто";
-                break;
-            case "100":
-                str_mul = "сто";
-                break;
-        }
-        return str_mul;
     }
 }
+
+
