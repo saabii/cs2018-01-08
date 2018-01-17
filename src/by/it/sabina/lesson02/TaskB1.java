9c17a75c8b2f4bf9e9162a69dd667b2ea727b12d
saabii <sbnmuller@gmail.com>
2018-01-17 11:42:22 +0300

    Task A1 complte Task B1 complite

--- src/by/it/sabina/lesson02/TaskB1.java
+++ src/by/it/sabina/lesson02/TaskB1.java
@@ -16,6 +16,21 @@ Scanner sc=new Scanner(System.in);
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Введите целое число");
+        int a=sc.nextInt();
+        int b=a*a;
+        System.out.println("Квадрат введённого числа + b");
+    }
+
+
+
+
+
 
 }
---
+++ src/by/it/sabina/lesson03/TaskB2.java
@@ -0,0 +1,59 @@
+package by.it.sabina.lesson03;
+
+import java.util.Scanner;
+
+/*
+Lesson 04. Task B2. Нужно написать программу, которая вводит три числа  a b c типа int с клавиатуры
+и вычисляет корни квадратного уравнения a*a*x + b*x + c = 0:
+1) Если два корня, то выводится два корня через пробел.
+2) Если один корень, то выводится только одно число - корень.
+3) Если нет действительных корней, то выводится сообщение об ошибке "Отрицательный дискриминант"
+
+В программе должен быть определен статический метод dis вычисления дискриминанта,
+который принимает на входе три числа типа int a,b,c
+а на выходе возвращает число типа double
+
+Примеры работы
+
+Ввод:
+2 5 3
+Вывод:
+-1.0 -1.5
+
+Ввод:
+2 4 2
+Вывод:
+-1.0
+
+Ввод:
+2 2 2
+Вывод:
+Отрицательный дискриминант
+
+*/
+class TaskB2 {
+static double dis (double a,double b, double c){
+    double d=b*b-4*a*c;
+    return d;
+
+    }
+
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        double c=sc.nextDouble();
+        double det=dis(a,b,c);
+        if (det>0) {
+                double x1=(-b+Math.sqrt(det))/(2*a);
+                double x2=(-b-Math.sqrt(det))/(2*a);
+                System.out.println("x1="+x1+ " x2="+x2);
+        }
+        else if (det==0) {
+            double x1=(-b)/(2*a);
+            System.out.println("Ввод:" + "\n" + (int)a +  " " + (int)b + " " + (int)c +  "\n" + "Вывод:" + "\n" + x1);
+        }
+        else
+            System.out.println("Ввод:" + "\n" + (int)a +  " " + (int)b + " " + (int)c +  "\n" + "Вывод:" + "\n" + "Отрицательный дискриминант");
+    }
+}
