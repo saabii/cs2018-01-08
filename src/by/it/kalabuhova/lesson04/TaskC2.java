3068a88119ebc71a6b0dbcbdea015e9f54a70f45
marilizka <kalabuhova@yandex.ru>
2018-01-17 11:33:44 +0300

    a1-c2 - complete

--- src/by/it/kalabuhova/lesson04/TaskC2.java
+++ src/by/it/kalabuhova/lesson04/TaskC2.java
@@ -1,5 +1,8 @@
 package by.it.kalabuhova.lesson04;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 На входе вводится число и месяц 2017 года.
 Программа должна выводить номер дня года, считая 1 января первым днем,
@@ -26,5 +29,48 @@ package by.it.kalabuhova.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int m = sc.nextInt();
+        switch (m - 1) {
+            case 0:
+                System.out.println(d);
+                break;
+            case 1:
+                System.out.println(31 + d);
+                break;
+            case 2:
+                System.out.println(59 + d);
+                break;
+            case 3:
+                System.out.println(90 + d);
+                break;
+            case 4:
+                System.out.println(120 + d);
+                break;
+            case 5:
+                System.out.println(151 + d);
+                break;
+            case 6:
+                System.out.println(181 + d);
+                break;
+            case 7:
+                System.out.println(212 + d);
+                break;
+            case 8:
+                System.out.println(243 + d);
+                break;
+            case 9:
+                System.out.println(273 + d);
+                break;
+            case 10:
+                System.out.println(304 + d);
+                break;
+            case 11:
+                System.out.println(334 + d);
+                break;
+        }
 
+    }
 }
--- src/by/it/kalabuhova/lesson04/TaskC3.java
+++ src/by/it/kalabuhova/lesson04/TaskC3.java
@@ -23,6 +23,91 @@ package by.it.kalabuhova.lesson04;
 десять умножить на десять равно сто
 */
 
+import java.util.function.IntToDoubleFunction;
+
 public class TaskC3 {
 
-}
+    public static void main(String[] args) {
+        /*class IntInText {
+                public final static String x1 = "один";
+                  public final static String x2 = "два";
+                  public final static String x3 = "три";
+                  public final static String x4 = "четыре";
+                  public final static String x5 = "пять";
+                  public final static String x6 = "шесть";
+                  public final static String x7 = "семь";
+                  public final static String x8 = "восемь";
+                  public final static String x9 = "девять";
+                  public final static String x10 = "десять";
+                  public final static String x11 = "одинадцать";
+                  public final static String x12 = "двенадцать";
+                  public final static String x13 = "тринадцать";
+                  public final static String x14 = "четырнадцать";
+                  public final static String x15 = "пятнадцать";
+                  public final static String x16 = "шестнадцать";
+                  public final static String x17 = "семнадцать";
+                  public final static String x18 = "восемнадцать";
+                  public final static String x19 = "девятнадцать";
+                  public final static String x20 = "двадцать";
+                  public final static String x30 = "тридцать";
+                  public final static String x40 = "сорок";
+                  public final static String x50 = "пятьдесят";
+                  public final static String x60 = "шестьдесят";
+                  public final static String x70 = "семьдесят";
+                  public final static String x80 = "восемдесят";
+                  public final static String x90 = "девяносто";
+                  public final static String x100 = "сто";
+              */
+            /*public static String ItS(int x) {
+                int i = 0;
+                String r;
+                for (i = 1; i <= 10; i++) {
+                    switch (i) {
+                        case 1:
+                            r = "один";
+                            break;
+                        case 2:
+                            r = "два";
+                            break;
+                        case 3:
+                            r = "три";
+                            break;
+                        case 4:
+                            r = "четыре";
+                            break;
+                        case 5:
+                            r = "пять";
+                            break;
+                        case 6:
+                            r = "шесть";
+                            break;
+                        case 7:
+                            r = "семь";
+                            break;
+                        case 8:
+                            r = "восемь";
+                            break;
+                        case 9:
+                            r = "девять";
+                            break;
+                        case 10:
+                            r = "десять";
+                            break;
+                        case 11:
+                            r = "одинадцать";
+                            break;
+                        case 12:
+                            r = "двенадцать";
+                            break;
+                        return r;
+                    }
+                }
+                for (int y = 1; y <= 10; y++) {
+                    for (int j = 1; j <= 10; j++)
+                        System.out.print(ItS(y) + "умножить на" + ItS(j) + "равно" + (y * j) + " ");
+                    System.out.println();
+                }
+            }
+        }
+    }*/
+}}
